30a18807bc76c741973a02cf4de7545ba77cde38
cchness <cness@riseup.net>
2021-10-30 15:29:28 -0300

    Adiciona permissão de escrita para outros usuários.

--- src/maketemplates/make_ref004a.mustache.mk
+++ src/maketemplates/make_ref004a.mustache.mk
@@ -71,7 +71,7 @@ wget_files:
 	@read _ENTER_OK_
 	mkdir -p $(orig)
 {{#files}}
-	@cd $(orig); wget http://preserv.addressforall.org/download/{{file}}
+	@cd $(orig); wget http://preserv.addressforall.org/download/{{file}} && chmod o+rw {{file}}
 {{/files}}
 	@echo "Please, if orig not default, run 'make _target_ orig=$(orig)'"
 
