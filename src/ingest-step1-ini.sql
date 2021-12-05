3067bccc4c5137b086ebe02f384b554691009dad
cchness <cness@riseup.net>
2021-12-04 23:36:17 -0300

    Reorganiza exemplos.

--- src/ingest-step1-ini.sql
+++ src/ingest-step1-ini.sql
@@ -1074,18 +1074,20 @@ CREATE or replace FUNCTION ingest.lix_insert(
         END CASE;    
     END;
 $wrap$ LANGUAGE PLpgSQL;
--- SELECT ingest.lix_insert('BR','/var/gits/_dg/preserv/src/maketemplates/make_ref027a.mustache.mk','mkme_srcTpl');
+--SELECT ingest.lix_insert('BR','/var/gits/_dg/preserv-BR/src/maketemplates/commomFirst.yaml','first_yaml');
+--SELECT ingest.lix_insert('BR','/var/gits/_dg/preserv-BR/src/maketemplates/readme.mustache','readme');
 
--- SELECT ingest.lix_insert('BR','/var/gits/_dg/preserv-BR/src/maketemplates/commomFirst.yaml','first_yaml');
--- SELECT ingest.lix_insert('BR','/var/gits/_dg/preserv-BR/src/maketemplates/readme.mustache','readme');
+--SELECT ingest.lix_insert('PE','/var/gits/_dg/preserv-PE/src/maketemplates/commomFirst.yaml','first_yaml');
+--SELECT ingest.lix_insert('PE','/var/gits/_dg/preserv-PE/src/maketemplates/readme.mustache','readme');
 
--- SELECT ingest.lix_insert('PE','/var/gits/_dg/preserv-PE/src/maketemplates/commomFirst.yaml','first_yaml');
--- SELECT ingest.lix_insert('PE','/var/gits/_dg/preserv-PE/src/maketemplates/readme.mustache','readme');
+--SELECT ingest.lix_insert('PE','/var/gits/_dg/preserv-CO/src/maketemplates/commomFirst.yaml','first_yaml');
+--SELECT ingest.lix_insert('PE','/var/gits/_dg/preserv-CO/src/maketemplates/readme.mustache','readme');
 
--- SELECT ingest.lix_insert('INT','/var/gits/_dg/preserv/src/maketemplates/commomLast.mustache.mk','mkme_srcTplLast');
+--SELECT ingest.lix_insert('INT','/var/gits/_dg/preserv/src/maketemplates/make_ref027a.mustache.mk','mkme_srcTpl');
+--SELECT ingest.lix_insert('INT','/var/gits/_dg/preserv/src/maketemplates/commomLast.mustache.mk','mkme_srcTplLast');
 
--- SELECT ingest.lix_insert('BR','/var/gits/_dg/preserv-BR/data/RJ/Niteroi/_pk018/make_conf.yaml','make_conf');
--- SELECT ingest.lix_insert('BR','/var/gits/_dg/preserv-BR/data/MG/BeloHorizonte/_pk012/make_conf.yaml','make_conf');
+--SELECT ingest.lix_insert('BR','/var/gits/_dg/preserv-BR/data/RJ/Niteroi/_pk018/make_conf.yaml','make_conf');
+--SELECT ingest.lix_insert('BR','/var/gits/_dg/preserv-BR/data/MG/BeloHorizonte/_pk012/make_conf.yaml','make_conf');
 
 
 CREATE or replace FUNCTION ingest.jsonb_mustache_prepare(
