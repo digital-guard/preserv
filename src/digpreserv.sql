b5ea48316bd1d180ef4011b73e7256a820440e77
cchness <cness@riseup.net>
2021-12-07 10:31:11 -0300

    Migra funções digpreserv_* de WS para preserv.

---
+++ src/digpreserv.sql
@@ -0,0 +1,79 @@
+-- system Digital Preservation
+
+
+CREATE or replace FUNCTION digpreserv_packid_to_real(pkid int, version int) RETURNS real AS $f$
+  SELECT pkid::real + (CASE WHEN version IS NULL or version<=0 THEN 1 ELSE version END)::real/1000.0::real
+  -- Estimativa de versões: 1 por semana ao longo de 15 anos. 15*12*4.3=780. Ainda sobram 320 por segurança.
+$f$ language SQL IMMUTABLE;
+COMMENT ON FUNCTION digpreserv_packid_to_real(int,int)
+ IS 'Encodes integer pkid and version into real pck_id, convention of the one new version a week for 15 years.'
+;
+CREATE or replace FUNCTION digpreserv_packid_to_real(pck_id int[]) RETURNS real AS $wrap$
+  SELECT digpreserv_packid_to_real(pck_id[1],pck_id[2])
+$wrap$ language SQL IMMUTABLE;
+COMMENT ON FUNCTION digpreserv_packid_to_real(int[])
+ IS 'Encodes integer array[pkid,version] into real pck_id, convention of the 20 years with 40 versions/year.'
+;
+
+CREATE or replace FUNCTION digpreserv_packid_to_real(str_pkid text) RETURNS real AS $f$
+  SELECT replace(str_pkid,'_','.')::real
+$f$ language SQL IMMUTABLE;
+COMMENT ON FUNCTION digpreserv_packid_to_real(text)
+ IS 'Converts pck_id format, text into real.'
+;
+
+CREATE or replace FUNCTION digpreserv_packid_to_ints(pck_id real) RETURNS int[] AS $f$
+  SELECT array[k::int,round((pck_id-k)*1000)::int]
+  FROM ( SELECT trunc(pck_id) k) t
+$f$ language SQL IMMUTABLE;
+COMMENT ON FUNCTION digpreserv_packid_to_ints(real)
+ IS 'Decodes real pck_id into integer array[pkid,version], convention of the 20 years with 40 versions/year.'
+;
+
+CREATE or replace FUNCTION digpreserv_packid_to_str(pck_id real, sep boolean default false) RETURNS text AS $f$
+ SELECT CASE WHEN sep IS NULL THEN replace(s,'.000','') WHEN sep THEN replace(s,'.','_') ELSE s END
+ FROM ( SELECT to_char(CASE WHEN sep IS NULL THEN floor($1) ELSE $1 END,'FM999999000.000') s ) t
+$f$ language SQL IMMUTABLE;
+
+CREATE or replace FUNCTION digpreserv_packid_to_str(pck_id int[], sep boolean default false) RETURNS text AS $wrap$
+  select  digpreserv_packid_to_str( digpreserv_packid_to_real($1), $2 )
+$wrap$ language SQL IMMUTABLE;
+
+CREATE or replace FUNCTION digpreserv_packid_to_str(pkid int, version int, sep boolean default false) RETURNS text AS $wrap$
+ select  digpreserv_packid_to_str( digpreserv_packid_to_real($1,$2), $3 )
+$wrap$ language SQL IMMUTABLE;
+
+CREATE or replace FUNCTION digpreserv_packid_plusone(pck_id real) RETURNS real AS $f$
+  SELECT digpreserv_packid_to_real(p[1],p[2]+1) -- +1?
+  FROM (SELECT digpreserv_packid_to_ints(pck_id) p) t
+$f$ language SQL IMMUTABLE;
+
+CREATE or replace FUNCTION digpreserv_packid_isvalid(pck_id real) RETURNS boolean AS $f$
+  SELECT CASE
+    WHEN p IS NULL OR p[1] IS NULL OR p[1]=0 OR p[2]=0 OR digpreserv_packid_to_real(p)!=pck_id::real THEN false
+    ELSE true
+    END
+  FROM (SELECT digpreserv_packid_to_ints(pck_id) p) t
+$f$ language SQL IMMUTABLE;
+
+-- falta dinâmico de MAX de real da tabela. Use digpreserv_packid_plusone(x) para o próximo.
+
+CREATE or replace FUNCTION digpreserv_packid_getmax(
+  p_tablename  text,
+  p_plusone boolean DEFAULT false
+) RETURNS real AS $f$
+DECLARE
+  r real;
+BEGIN
+  EXECUTE format(
+    CASE
+      WHEN p_plusone THEN 'SELECT MAX(pck_id) INTO r FROM %s'
+      ELSE  'SELECT digpreserv_packid_plusone(MAX(pck_id)) INTO r FROM %s'
+    END, p_tablename
+  );
+  RETURN r;
+END
+$f$ LANGUAGE PLpgSQL;
+COMMENT ON FUNCTION digpreserv_packid_getmax
+ IS 'Obtais the current (or the next when p_plusOne) pck_id of a table.'
+;
