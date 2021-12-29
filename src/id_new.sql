90e92d8a68f41cff347f36ff34322be647365df6
cchness <cness@riseup.net>
2021-12-29 18:32:38 -0300

    insert prelimimar de donated_PackTpl.

--- src/id_new.sql
+++ src/id_new.sql
@@ -12,3 +12,23 @@ SELECT ingest.fdw_generate_getclone('donor', null, 'optim', array['id','country_
 
 
 
+
+
+CREATE FUNCTION optim.mkdonated_PackTpl() RETURNS TRIGGER AS $f$
+BEGIN
+  NEW.kx_num_files = jsonb_array_length(NEW.make_conf_tpl->files);
+	RETURN NEW;
+END;
+$f$ LANGUAGE PLpgSQL;
+CREATE TRIGGER check_kx_num_files
+    BEFORE INSERT OR UPDATE ON optim.donated_PackTpl
+    FOR EACH ROW EXECUTE PROCEDURE optim.mkdonated_PackTpl()
+;
+
+-- popula optim.donated_PackTpl a partir de tmp_orig.fdw_donatedPack_br
+INSERT INTO optim.donated_PackTpl (donor_id, user_resp, pk_count, original_tpl, make_conf_tpl)
+SELECT (SELECT jurisd_base_id*1000000+donor_id FROM optim.jurisdiction WHERE isolabel_ext = split_part(escopo, '-', 1)), user_resp, 1, pg_read_file('/var/gits/_dg/preserv-'|| replace(regexp_replace(escopo, '-', '/data/'),'-',$$/$$) || '/_pk' || to_char(donor_id,'fm0000') || '.' || to_char(1,'fm00') || '/make_conf.yaml'), yamlfile_to_jsonb('/var/gits/_dg/preserv-'|| replace(regexp_replace(escopo, '-', '/data/'),'-',$$/$$) || '/_pk' || to_char(donor_id,'fm0000') || '.' || to_char(1,'fm00') || '/make_conf.yaml') as make_conf_tpl
+FROM optim.donatedpack;
+--ON CONFLICT (donor_id,pk_count)
+--DO UPDATE 
+--SET ;
