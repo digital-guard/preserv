b5871766f2d4c0d691963549cebc89f895068960
peter krauss <ppkrauss@gmail.com>
2022-02-11 07:34:02 -0300

    add new DL redirector after Lua crash

---
+++ src/dl.php
@@ -0,0 +1,17 @@
+<?php
+$json_url = "http://127.0.0.1:3102/redirects?fhash=like.".str_replace("/","",$_SERVER['REQUEST_URI'])."*";
+$json     = file_get_contents($json_url);
+$data     = json_decode($json,TRUE);
+
+if (isset($data[0]['fhash'])) {
+    if (isset($data[0]['furi'])) {
+        header("Location: ".$data[0]['furi']);
+    }
+    else {
+        header("Location: /download/".$data[0]['fhash']);
+    }
+}
+else {
+    header("Location: /download".$_SERVER['REQUEST_URI']);
+}
+?>
