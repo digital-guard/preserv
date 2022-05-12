720602c768fab846f3d6999f0a76ce8637996615
0e1 <0e1@riseup.net>
2022-05-12 14:43:12 -0300

    id serial not_approved. digital-guard/preserv-BR#93

--- src/to_donated_packcomponent.bash
+++ src/to_donated_packcomponent.bash
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sed -i 's/\(INSERT INTO.*\);$/\1 ON CONFLICT (packvers_id,ftid,lineage_md5) DO UPDATE SET (is_evidence, proc_step, lineage, kx_profile) = (EXCLUDED.is_evidence, EXCLUDED.proc_step, EXCLUDED.lineage, EXCLUDED.kx_profile);/g' $1
-sed -i 's/ingest\.donated_packcomponent/optim\.donated_PackComponent_not_approved/g' $1
+sed -i 's/\(INSERT INTO\) ingest.donated_packcomponent (id, \(packvers_id, ftid, is_evidence, proc_step, lineage, lineage_md5, kx_profile) VALUES (\)[0-9]*,\(.*\);$/\1 optim\.donated_PackComponent_not_approved (\2\3  ON CONFLICT (packvers_id,ftid,lineage_md5) DO UPDATE SET (is_evidence, proc_step, lineage, kx_profile) = (EXCLUDED.is_evidence, EXCLUDED.proc_step, EXCLUDED.lineage, EXCLUDED.kx_profile);/g' $1
