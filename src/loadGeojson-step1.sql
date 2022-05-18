e6b1a4066f29aa934b9e1428ac9ebcdb2444cecd
0e1 <0e1@riseup.net>
2022-05-18 13:27:02 -0300

    Ajustes.

--- src/loadGeojson-step1.sql
+++ src/loadGeojson-step1.sql
@@ -1,6 +1,7 @@
-CREATE or replace FUNCTION geojson_readfile_cutgeo(file text, packvers_id bigint) RETURNS TABLE (
-  file_id int, feature_id int, properties jsonb, geom geometry
-) AS $f$
+CREATE or replace FUNCTION geojson_readfile_cutgeo(
+    p_file text,
+    p_packvers_id bigint
+) RETURNS TABLE ( file_id int, feature_id int, properties jsonb, geom geometry ) AS $f$
     SELECT 
         file_id,
         subfeature_id+COALESCE(
@@ -17,18 +18,18 @@ CREATE or replace FUNCTION geojson_readfile_cutgeo(file text, packvers_id bigint
             (
                 SELECT id
                 FROM ingest.donated_packcomponent
-                WHERE packvers_id=packvers_id
+                WHERE packvers_id=p_packvers_id
                 LIMIT 1
             ) AS file_id,
             (ROW_NUMBER() OVER())::int AS subfeature_id,
-            subfeature->'properties' AS properties,
+            subfeature->'properties'   AS properties,
             ST_GeomFromGeoJSON( '{"crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:EPSG::4326"}}}'::jsonb || (subfeature->'geometry') ) AS geom
         FROM
         (
             SELECT jsonb_array_elements(j->'features') AS subfeature
             FROM 
                 (
-                    SELECT pg_read_file(file)::jsonb AS j
+                    SELECT pg_read_file(p_file)::jsonb AS j
                 ) jfile
         ) t2
     ) t3
