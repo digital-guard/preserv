65aee68e8e80c84b3666f0f64467934a2fef33e6
0e1 <0e1@riseup.net>
2022-06-21 19:04:30 -0300

    api para stats de pacotes doados. #113

--- src/optim-step4-api.sql
+++ src/optim-step4-api.sql
@@ -15,3 +15,80 @@ FROM optim.jurisdiction
 ;
 
 --curl "http://localhost:3103/jurisdiction?jurisd_base_id=eq.76&parent_abbrev=eq.CE" -H "Accept: text/csv"
+
+
+CREATE or replace VIEW api.stats_donated_packcomponent AS
+SELECT *
+FROM (
+      SELECT pc.id,
+      pc.packvers_id,
+      pc.proc_step,
+      pc.ftid,
+      pc.is_evidence,
+      (lineage->'statistics')[0]                                  AS quantidade_feicoes_bruta,
+      pc.kx_profile->'date_aprroved'                              AS date_aprroved,
+      pc.kx_profile->'publication_summary'->'size'                AS size,
+      pc.kx_profile->'publication_summary'->'bytes'               AS bytes,
+      pc.kx_profile->'publication_summary'->'itens'               AS itens,
+      pc.kx_profile->'publication_summary'->'size_unit'           AS size_unit,
+      pc.kx_profile->'publication_summary'->'avg_density'         AS avg_density,
+      pc.kx_profile->'publication_summary'->'size_unitDensity'    AS size_unitDensity,
+      ft.ftname,
+      split_part(ft.ftname,'_',1)                                 AS ftname_class,
+      ft.geomtype,
+      ft.need_join,
+      ft.description
+      FROM optim.donated_PackComponent pc
+      LEFT JOIN optim.feature_type ft
+      ON ft.ftid = pc.ftid
+      --WHERE pc.ftid > 19
+) a
+INNER JOIN
+(
+      SELECT *
+      FROM tmp_orig.fdw_donorbr d
+      INNER JOIN tmp_orig.fdw_donatedpackbr p
+      ON d.local_id::int = p.donor_id
+) b
+ON substring(to_char(a.packvers_id,'FM00000000000000'),4,8) = to_char(b.pack_id,'FM00000000')
+    AND substring(to_char(a.packvers_id,'FM00000000000000'),1,3) = '076'
+
+UNION ALL
+
+SELECT *
+FROM (
+      SELECT pc.id,
+      pc.packvers_id,
+      pc.proc_step,
+      pc.ftid,
+      pc.is_evidence,
+      (lineage->'statistics')[0]                                  AS quantidade_feicoes_bruta,
+      pc.kx_profile->'date_aprroved'                              AS date_aprroved,
+      pc.kx_profile->'publication_summary'->'size'                AS size,
+      pc.kx_profile->'publication_summary'->'bytes'               AS bytes,
+      pc.kx_profile->'publication_summary'->'itens'               AS itens,
+      pc.kx_profile->'publication_summary'->'size_unit'           AS size_unit,
+      pc.kx_profile->'publication_summary'->'avg_density'         AS avg_density,
+      pc.kx_profile->'publication_summary'->'size_unitDensity'    AS size_unitDensity,
+      ft.ftname,
+      split_part(ft.ftname,'_',1)                                 AS ftname_class,
+      ft.geomtype,
+      ft.need_join,
+      ft.description
+      FROM optim.donated_PackComponent pc
+      LEFT JOIN optim.feature_type ft
+      ON ft.ftid = pc.ftid
+      --WHERE pc.ftid > 19
+) a
+INNER JOIN
+(
+      SELECT r.*, null, null, s.*
+      FROM tmp_orig.fdw_donorco r
+      INNER JOIN tmp_orig.fdw_donatedpackco s
+      ON r.local_id::int = s.donor_id
+) b
+ON substring(to_char(a.packvers_id,'FM00000000000000'),4,8) = to_char(b.pack_id,'FM00000000')
+    AND substring(to_char(a.packvers_id,'FM00000000000000'),1,3) = '170'
+;
+
+--curl "http://localhost:3103/stats_donated_packcomponent?uri_objtype=like.*email*" -H "Accept: text/csv"
