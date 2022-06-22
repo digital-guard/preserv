2f879aa1261db36b508616033ed7803f345d6d48
0e1 <0e1@riseup.net>
2022-06-22 16:52:34 -0300

    api urn lex.

--- src/optim-step4-api.sql
+++ src/optim-step4-api.sql
@@ -13,9 +13,9 @@ SELECT osm_id,
       jsonb_strip_nulls(info)
 FROM optim.jurisdiction
 ;
-
 --curl "http://localhost:3103/jurisdiction?jurisd_base_id=eq.76&parent_abbrev=eq.CE" -H "Accept: text/csv"
 
+---------
 
 CREATE or replace VIEW api.stats_donated_packcomponent AS
 SELECT *
@@ -90,5 +90,105 @@ INNER JOIN
 ON substring(to_char(a.packvers_id,'FM00000000000000'),4,8) = to_char(b.pack_id,'FM00000000')
     AND substring(to_char(a.packvers_id,'FM00000000000000'),1,3) = '170'
 ;
-
 --curl "http://localhost:3103/stats_donated_packcomponent?uri_objtype=like.*email*" -H "Accept: text/csv"
+
+---------
+
+CREATE or replace VIEW api.jurisdiction_lexlabel AS
+SELECT isolabel_ext,
+CASE
+    WHEN cardinality(a)=3 THEN lower(a[1] || ';' ||  lexlabel_parent || ';' || lexlabel)
+    WHEN cardinality(a)=2 THEN lower(a[1] || ';' || lexlabel)
+    WHEN cardinality(a)=1 THEN lower(isolabel_ext)
+    ELSE NULL
+END AS lex_isoinlevel1,
+CASE
+    WHEN cardinality(a)=3 THEN lower(a[1] || ';' ||  a[2] || ';' || lexlabel)
+    WHEN cardinality(a)=2 THEN lower(a[1] || ';' || lexlabel)
+    WHEN cardinality(a)=1 THEN lower(isolabel_ext)
+    ELSE NULL
+END AS lex_isoinlevel2,
+CASE
+    WHEN cardinality(a)=3 THEN lower(a[1] || ';' ||  a[2] || ';' || abbrev)
+    WHEN cardinality(a)=2 THEN lower(a[1] || ';' ||  a[2])
+    WHEN cardinality(a)=1 THEN lower(isolabel_ext)
+    ELSE NULL
+END AS lex_isoinlevel2_abbrev
+FROM (
+    SELECT s.isolabel_ext AS isolabel_ext_parent, s.lexlabel AS lexlabel_parent, r.isolabel_ext, r.abbrev, r.name, r.lexlabel, regexp_split_to_array (r.isolabel_ext,'(-)')::text[] AS a
+    FROM optim.vw01full_jurisdiction_geom r
+    LEFT JOIN optim.jurisdiction s
+    ON s.isolabel_ext = (SELECT a[1]||'-'||a[2] FROM regexp_split_to_array (r.isolabel_ext,'(-)') a)
+) t
+;
+
+CREATE or replace FUNCTION api.jurisdiction_geojson_from_isolabel(
+   p_isolabel_ext text
+) RETURNS jsonb AS $f$
+    SELECT jsonb_build_object(
+        'type', 'FeatureCollection',
+        'features',
+            (
+                ST_AsGeoJSONb(
+                    geom,
+                    6,0,null,
+                    jsonb_build_object(
+                        'osm_id', osm_id,
+                        'jurisd_base_id', jurisd_base_id,
+                        'jurisd_local_id', jurisd_local_id,
+                        'parent_id', parent_id,
+                        'admin_level', admin_level,
+                        'name', name,
+                        'parent_abbrev', parent_abbrev,
+                        'abbrev', abbrev,
+                        'wikidata_id', wikidata_id,
+                        'lexlabel', lexlabel,
+                        'isolabel_ext', isolabel_ext,
+                        'lex_urn', lex_urn,
+                        'name_en', name_en,
+                        'isolevel', isolevel,
+                        'area', ST_Area(geom,true),
+                        'jurisd_base_id', jurisd_base_id
+                        )
+                    )::jsonb
+            )
+        )
+    FROM optim.vw01full_jurisdiction_geom
+    WHERE isolabel_ext = p_isolabel_ext
+$f$ LANGUAGE SQL IMMUTABLE;
+COMMENT ON FUNCTION api.jurisdiction_geojson_from_isolabel(text)
+  IS 'Return jurisdiction geojson from isolabel_ext.'
+;
+--SELECT api.jurisdiction_geojson_from_isolabel('BR-SP-Campinas');
+
+
+CREATE or replace FUNCTION api.jurisdiction_geojson_from_lex_isoinlevel1(
+   p_lex text
+) RETURNS jsonb AS $f$
+    SELECT api.jurisdiction_geojson_from_isolabel(( SELECT isolabel_ext FROM api.jurisdiction_lexlabel WHERE lex_isoinlevel1 = lower(p_lex) ))
+$f$ LANGUAGE SQL IMMUTABLE;
+COMMENT ON FUNCTION api.jurisdiction_geojson_from_lex_isoinlevel1(text)
+  IS 'Return jurisdiction geojson from lex. ISO 3166-1 alpha-2 country code.'
+;
+--SELECT api.jurisdiction_geojson_from_lex_isoinlevel1('br;sao.paulo;campinas');
+
+CREATE or replace FUNCTION api.jurisdiction_geojson_from_lex_isoinlevel2(
+   p_lex text
+) RETURNS jsonb AS $f$
+    SELECT api.jurisdiction_geojson_from_isolabel(( SELECT isolabel_ext FROM api.jurisdiction_lexlabel WHERE lex_isoinlevel2 = lower(p_lex) ))
+$f$ LANGUAGE SQL IMMUTABLE;
+COMMENT ON FUNCTION api.jurisdiction_geojson_from_lex_isoinlevel2(text)
+  IS 'Return jurisdiction geojson from lex. ISO 3166-1 alpha-2 code.'
+;
+--SELECT api.jurisdiction_geojson_from_lex_isoinlevel2('br;sp;campinas');
+
+CREATE or replace FUNCTION api.jurisdiction_geojson_from_lex_isoinlevel2_abbrev(
+   p_lex text
+) RETURNS jsonb AS $f$
+    SELECT api.jurisdiction_geojson_from_isolabel(( SELECT isolabel_ext FROM api.jurisdiction_lexlabel WHERE lex_isoinlevel2_abbrev = lower(p_lex) ))
+$f$ LANGUAGE SQL IMMUTABLE;
+COMMENT ON FUNCTION api.jurisdiction_geojson_from_lex_isoinlevel2_abbrev(text)
+  IS 'Return jurisdiction geojson from lex. All abbrev.'
+;
+--SELECT api.jurisdiction_geojson_from_lex_isoinlevel2_abbrev('br;sp;cam');
+--SELECT api.jurisdiction_geojson_from_lex_isoinlevel2_abbrev('br;sp');
