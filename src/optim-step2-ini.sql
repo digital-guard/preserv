32ee30b07de3e86808510156ed3435b1ea1a651f
0e1 <0e1@riseup.net>
2022-07-01 01:39:53 -0300

    update em listas.

--- src/optim-step2-ini.sql
+++ src/optim-step2-ini.sql
@@ -1,2 +1,2 @@
-SELECT optim.load_donor_pack(t)   FROM unnest(ARRAY['AR','BO','BR','CL','CO','EC','PE','PY','SR','UY','VE']) t;
+--SELECT optim.load_donor_pack(t)   FROM unnest(ARRAY['AR','BO','BR','CL','CO','EC','PE','PY','SR','UY','VE']) t;
 SELECT optim.insert_donor_pack(t) FROM unnest(ARRAY['AR','BO','BR','CL','CO','EC','PE','PY','SR','UY','VE']) t;
