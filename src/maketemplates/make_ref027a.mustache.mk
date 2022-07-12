986ebbe33c7434a47a6f3583f12b042c58ae747c
0e1 <0e1@riseup.net>
2022-07-12 18:50:10 -0300

    Add datagrid. preserv#115

--- src/maketemplates/make_ref027a.mustache.mk
+++ src/maketemplates/make_ref027a.mustache.mk
@@ -205,6 +205,21 @@ via-clean:
 
 {{>common008_publicating_geojsons}}
 {{/via}}
+
+{{#datagrid}}
+datagrid: tabname = {{tabname}}
+datagrid: makedirs
+{{>common002_layerHeader}}
+{{>common003_shp2pgsql}}
+{{>common001_pgAny_load}}
+{{>common007_layerFooter}}
+
+datagrid-clean:
+{{>common006_clean}}
+
+{{>common008_publicating_geojsons}}
+{{/datagrid}}
+
 {{/layers}}
 
 {{#openstreetmap}}
