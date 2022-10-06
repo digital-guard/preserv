a7014491e52822b637ff315fdfe3be67307923a4
0e1 <0e1@riseup.net>
2022-10-06 16:27:31 -0300

    Move joins to end.

--- src/maketemplates/reproducibility/make_ref027a.mustache.mk
+++ src/maketemplates/reproducibility/make_ref027a.mustache.mk
@@ -70,9 +70,6 @@ mkdir -p {{pg_io}}
 {{>common002_layerHeader}}
 {{>common003_shp2pgsql}}
 {{>common001_pgAny_load}}
-{{#joins}}{{#genericvia}}
-{{>common005_join}}
-{{/genericvia}}{{/joins}}
 {{>common006_clean}}
 {{>common008_publicating_geojsons}}
 {{/genericvia}}
@@ -89,9 +86,6 @@ mkdir -p {{pg_io}}
 psql {{pg_uri}}/{{pg_db}} -c "CREATE VIEW vw{{file}}_{{tabname}} AS SELECT way, tags - ARRAY['addr:housenumber','addr:street'] || jsonb_objslice(ARRAY['addr:housenumber','addr:street'], tags, ARRAY['house_number','via_name']) AS tags FROM jplanet_osm_point WHERE tags ?| ARRAY['addr:housenumber','addr:street'] AND country_id = {{country_id}}::smallint UNION ALL SELECT ST_PointOnSurface(way) AS way, tags - ARRAY['addr:housenumber','addr:street'] || jsonb_objslice(ARRAY['addr:housenumber','addr:street'], tags, ARRAY['house_number','via_name']) AS tags FROM jplanet_osm_polygon WHERE tags ?| ARRAY['addr:housenumber','addr:street'] AND tags ?& ARRAY['building'] AND country_id = {{country_id}}::smallint "
 {{/isOsm}}
 {{>common001_pgAny_load}}
-{{#joins}}{{#geoaddress}}
-{{>common005_join}}
-{{/geoaddress}}{{/joins}}
 {{>common006_clean}}
 {{>common008_publicating_geojsons}}
 {{/geoaddress}}
@@ -118,9 +112,6 @@ mkdir -p {{pg_io}}
 {{>common002_layerHeader}}
 {{>common003_shp2pgsql}}
 {{>common001_pgAny_load}}
-{{#joins}}{{#parcel}}
-{{>common005_join}}
-{{/parcel}}{{/joins}}
 {{>common006_clean}}
 {{>common008_publicating_geojsons}}
 {{/parcel}}
@@ -137,9 +128,6 @@ mkdir -p {{pg_io}}
 psql {{pg_uri}}/{{pg_db}} -c "CREATE VIEW vw{{file}}_{{tabname}} AS SELECT way, tags - ARRAY['name'] || jsonb_objslice(ARRAY['name'], tags, ARRAY['via_name']) AS tags FROM jplanet_osm_line WHERE tags->>'highway' IN ('residential','unclassified','tertiary','secondary','primary','trunk','motorway') AND country_id = {{country_id}}::smallint "
 {{/isOsm}}
 {{>common001_pgAny_load}}
-{{#joins}}{{#via}}
-{{>common005_join}}
-{{/via}}{{/joins}}
 {{>common006_clean}}
 {{>common008_publicating_geojsons}}
 {{/via}}
@@ -169,3 +157,8 @@ osm2pgsql -E {{srid}} -c -d {{pg_db}} -U postgres -H localhost --slim --hstore -
 psql {{pg_uri}}/{{pg_db}} -c "SELECT ingest.jplanet_inserts_and_drops({{country_id}}::smallint,true);"
 {{>common006_clean}}
 {{/openstreetmap}}
+
+{{#joins}}{{#genericvia}}{{>common005_join}}{{/genericvia}}
+{{#geoaddress}}{{>common005_join}}{{/geoaddress}}
+{{#via}}{{>common005_join}}{{/via}}
+{{#parcel}}{{>common005_join}}{{/parcel}}{{/joins}}
