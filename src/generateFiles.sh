6182ebd7bc12a7112550d3ca292c1978cc61f0a2
0e1 <0e1@riseup.net>
2022-12-21 11:34:37 -0300

    Lida com caso de propriets = {}.

--- src/generateFiles.sh
+++ src/generateFiles.sh
@@ -13,7 +13,7 @@ update_tables(){
     ftid=$(cut -d'|' -f2 <<< ${id_pack})
     lineage_md5=$(cut -d'|' -f3 <<< ${id_pack})
 
-    psql postgres://postgres@localhost/dl03t_main -c"INSERT INTO optim.donated_PackComponent_cloudControl(packvers_id,ftid,lineage_md5,hashedfname,hashedfnameuri,hashedfnametype) VALUES (${packvers_id},${ftid},'${lineage_md5}','${file_name_hash}','${url_cloud}','${file_type}');" && psql postgres://postgres@localhost/dl02s_main -c"INSERT INTO download.redirects(fhash,furi) VALUES ('${file_name_hash}','${url_cloud}');"
+    psql postgres://postgres@localhost/dl03t_main -c"INSERT INTO optim.donated_PackComponent_cloudControl(packvers_id,ftid,lineage_md5,hashedfname,hashedfnameuri,hashedfnametype) VALUES (${packvers_id},${ftid},'${lineage_md5}','${file_name_hash}','${url_cloud}','${file_type}');" && psql postgres://postgres@localhost/dl02s_main -c"INSERT INTO download.redirects(fhash,furi) VALUES ('${file_name_hash}','${url_cloud}') ;"
 }
 
 # gen_shapefile a4a_co_pk0006_02_geoaddress ingest99 1
@@ -25,7 +25,7 @@ gen_shapefile(){
 
     pushd /tmp/
 
-    pgsql2shp -k -f ${file_basename}.shp -h localhost -u postgres -P postgres ${database} "$(psql postgres://postgres@localhost/${database} -qtAX -c "SELECT 'SELECT feature_id AS gid,' || array_to_string((SELECT  array_agg(('properties->''' || x || ''' AS ' || x)) FROM jsonb_object_keys((SELECT properties FROM ingest.feature_asis WHERE file_id=${file_id} LIMIT 1)) t(x)),', ') || ', geom FROM ingest.feature_asis WHERE file_id=${file_id};';")"
+    pgsql2shp -k -f ${file_basename}.shp -h localhost -u postgres -P postgres ${database} "$(psql postgres://postgres@localhost/${database} -qtAX -c "SELECT 'SELECT feature_id AS gid,' || CASE WHEN (SELECT properties FROM ingest.feature_asis WHERE file_id=${file_id} LIMIT 1) = '{}'::jsonb THEN '' ELSE array_to_string((SELECT  array_agg(('properties->''' || x || ''' AS ' || x)) FROM jsonb_object_keys((SELECT properties FROM ingest.feature_asis WHERE file_id=${file_id} LIMIT 1)) t(x)),', ') || ', ' END || ', geom FROM ingest.feature_asis WHERE file_id=${file_id};';")"
 
     mkdir ${file_basename}
     mv ${file_basename}.{shp,cpg,dbf,prj,shx} ${file_basename}
@@ -58,16 +58,16 @@ gen_csv(){
     database=$2
     file_id=$3
 
-    pushd /tmp
+    pushd /tmp/pg_io
 
-    CMD_STRING=$(psql postgres://postgres@localhost/${database} -qtAX -c "SELECT 'SELECT feature_id AS gid,' || array_to_string((SELECT  array_agg(('properties->''' || x || ''' AS ' || x)) FROM jsonb_object_keys((SELECT properties FROM ingest.feature_asis WHERE file_id=${file_id} LIMIT 1)) t(x)),', ') || ', ST_X(geom) AS longitude, ST_Y(geom) AS latitude FROM ingest.feature_asis WHERE file_id=${file_id}'")
+    CMD_STRING=$(psql postgres://postgres@localhost/${database} -qtAX -c "SELECT 'SELECT feature_id AS gid,' || CASE WHEN (SELECT properties FROM ingest.feature_asis WHERE file_id=${file_id} LIMIT 1) = '{}'::jsonb THEN '' ELSE array_to_string((SELECT  array_agg(('properties->''' || x || ''' AS ' || x)) FROM jsonb_object_keys((SELECT properties FROM ingest.feature_asis WHERE file_id=${file_id} LIMIT 1)) t(x)),', ') || ', ' END || ' ST_X(geom) AS longitude, ST_Y(geom) AS latitude FROM ingest.feature_asis WHERE file_id=${file_id}'")
 
     COPY_STRING=$(echo COPY \( ${CMD_STRING} ORDER BY feature_id \) TO \'/tmp/pg_io/${file_basename}.csv\' CSV HEADER)
 
     psql postgres://postgres@localhost/${database} -c "${COPY_STRING}"
 
     file_name="${file_basename}.csv.zip"
-    zip -r ${file_name} /tmp/pg_io/${file_basename}.csv
+    zip -r ${file_name} ${file_basename}.csv
     file_sha256sum=$( sha256sum -b ${file_name} | cut -f1 -d' ' )
     file_name_hash="${file_sha256sum}.zip"
     mv ${file_name} ${file_name_hash}
