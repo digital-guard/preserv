e916a4f0195f79100f8dc089acdf197750a91f5d
0e1 <0e1@riseup.net>
2023-03-01 15:21:55 -0300

    Repete feature_id na coluna fid quando layer não posuir atributos alem da geometria. bug do pgsql2shp.

--- src/generateFiles.sh
+++ src/generateFiles.sh
@@ -28,7 +28,7 @@ gen_shapefile(){
     pushd /tmp/
 
     echo "Generating shapefile..."
-    pgsql2shp -k -f ${file_basename}.shp -h localhost -u postgres -P postgres ${database} "$(psql postgres://postgres@localhost/${database} -qtAX -c "SELECT 'SELECT ' || array_to_string((SELECT ARRAY['feature_id AS gid'] || array_agg(('properties->>''' || x || ''' AS ' || x)) FROM jsonb_object_keys((SELECT properties FROM ingest.feature_asis WHERE file_id=${file_id} LIMIT 1)) t(x) WHERE x IN ('via','hnum','nsvia','name')),', ') || ', geom FROM ingest.feature_asis WHERE file_id=${file_id};';")"
+    pgsql2shp -k -f ${file_basename}.shp -h localhost -u postgres -P postgres ${database} "$(psql postgres://postgres@localhost/${database} -qtAX -c "SELECT 'SELECT ' || array_to_string((SELECT ARRAY['feature_id AS gid'] || array_agg(('properties->>''' || x || ''' AS ' || x)) FROM jsonb_object_keys((SELECT properties FROM ingest.feature_asis WHERE file_id=${file_id} LIMIT 1)) t(x) WHERE x IN ('via','hnum','nsvia','name')),', ') || (CASE WHEN (SELECT properties FROM ingest.feature_asis WHERE file_id=${file_id} LIMIT 1) ?| ARRAY['via','hnum','name','nsvia'] THEN '' ELSE ',feature_id AS fid' END) || ', geom FROM ingest.feature_asis WHERE file_id=${file_id};';")"
 
     mkdir ${file_basename}
     mv ${file_basename}.{shp,cpg,dbf,prj,shx} ${file_basename}
