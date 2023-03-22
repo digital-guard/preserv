386e5afd869d2399b4dcaa8df955ed3c82df4d12
0e1 <0e1@riseup.net>
2023-03-22 15:29:13 -0300

    Consome de fromDL_toFileServer.csv. AddressForAll/site-v2#48

--- src/download-step1-def.sql
+++ src/download-step1-def.sql
@@ -20,20 +20,32 @@ COMMENT ON TABLE download.redirects
 ;
 CREATE INDEX redirects_hashedfname_idx1 ON download.redirects USING btree (hashedfname);
 
-CREATE or replace VIEW api.redirects AS
-    -- arquivos originais
-    SELECT hashedfname AS fhash, hashedfnameuri AS furi
-    FROM download.redirects
-
-    UNION
-
-    -- arquivos filtrados
-    SELECT hashedfname, hashedfnameuri
-    FROM optim.donated_PackComponent_cloudControl
+CREATE or replace FUNCTION download.load_dldg_csv(
+    p_filename text DEFAULT '/var/gits/_dg/preserv/data/redirs/fromDL_toFileServer.csv'
+) RETURNS text AS $f$
+  SELECT optim.fdw_generate_direct_csv(p_filename,'tmp_orig.redirects_dlguard',',');
+$f$ LANGUAGE SQL;
+COMMENT ON FUNCTION download.load_dldg_csv
+  IS 'Generates a clone-structure FOREIGN TABLE for fromDL_toFileServer.csv.'
 ;
-COMMENT ON VIEW api.redirects
-  IS ''
+-- SELECT download.load_dldg_csv('/var/gits/_dg/preserv/data/redirs/fromDL_toFileServer.csv');
+-- SELECT optim.fdw_generate_direct_csv('/var/gits/_dg/preserv/data/redirs/fromDL_toFileServer.csv','tmp_orig.redirects_viz',',');
+
+CREATE or replace FUNCTION download.insert_dldg_csv(
+) RETURNS text AS $f$
+  INSERT INTO download.redirects(donor_id,filename_original,package_path,hashedfname,hashedfnameuri)
+  SELECT donor_id,filename_original,package_path,de_sha256,para_url
+  FROM tmp_orig.redirects_dlguard
+  ON CONFLICT (hashedfname,hashedfnameuri)
+  DO UPDATE
+  SET donor_id=EXCLUDED.donor_id, filename_original=EXCLUDED.filename_original, package_path=EXCLUDED.package_path
+  RETURNING 'Ok, updated download.redirects.'
+  ;
+$f$ LANGUAGE SQL;
+COMMENT ON FUNCTION download.insert_dldg_csv
+  IS 'Update download.redirects from tmp_orig.redirects_dlguard'
 ;
+-- SELECT download.insert_dldg_csv();
 
 ----------------------
 
@@ -53,15 +65,15 @@ COMMENT ON TABLE download.redirects_viz
 CREATE INDEX redirects_viz_jurisdiction_pack_layer_idx1 ON download.redirects_viz USING btree (jurisdiction_pack_layer);
 
 CREATE or replace FUNCTION download.load_viz_csv(
-    p_filename text DEFAULT '/var/gits/_dg/preserv/data/viz/fromCutLayer_toVizLayer.csv'
+    p_filename text DEFAULT '/var/gits/_dg/preserv/data/redirs/fromCutLayer_toVizLayer.csv'
 ) RETURNS text AS $f$
   SELECT optim.fdw_generate_direct_csv(p_filename,'tmp_orig.redirects_viz',',');
 $f$ LANGUAGE SQL;
 COMMENT ON FUNCTION download.load_viz_csv
   IS 'Generates a clone-structure FOREIGN TABLE for fromCutLayer_toVizLayer.csv.'
 ;
--- SELECT download.load_viz_csv('/var/gits/_dg/preserv/data/viz/fromCutLayer_toVizLayer.csv');
--- SELECT optim.fdw_generate_direct_csv('/var/gits/_dg/preserv/data/viz/fromCutLayer_toVizLayer.csv','tmp_orig.redirects_viz',',');
+-- SELECT download.load_viz_csv('/var/gits/_dg/preserv/data/redirs/fromCutLayer_toVizLayer.csv');
+-- SELECT optim.fdw_generate_direct_csv('/var/gits/_dg/preserv/data/redirs/fromCutLayer_toVizLayer.csv','tmp_orig.redirects_viz',',');
 
 CREATE or replace FUNCTION download.insert_viz_csv(
 ) RETURNS text AS $f$
@@ -102,3 +114,20 @@ COMMENT ON FUNCTION api.redirects_viz(text)
   IS ''
 ;
 -- SELECT api.redirects_viz('BR-SP-Jacarei/_pk0145.01/parcel');
+
+----------------------
+
+CREATE or replace VIEW api.redirects AS
+    -- dl.digital-guard
+    SELECT hashedfname AS fhash, hashedfnameuri AS furi
+    FROM download.redirects
+
+    UNION
+
+    -- Data VisualiZation
+    SELECT hashedfname, hashedfnameuri
+    FROM optim.donated_PackComponent_cloudControl
+;
+COMMENT ON VIEW api.redirects
+  IS ''
+;
--- src/download-step2-ini.sql
+++ src/download-step2-ini.sql
@@ -1,2 +1,7 @@
-SELECT download.load_viz_csv('/var/gits/_dg/preserv/data/viz/fromCutLayer_toVizLayer.csv');
+-- dl.digital-guard
+SELECT download.load_dldg_csv('/var/gits/_dg/preserv/data/redirs/fromDL_toFileServer.csv');
+SELECT download.insert_dldg_csv();
+
+-- Data VisualiZation
+SELECT download.load_viz_csv('/var/gits/_dg/preserv/data/redirs/fromCutLayer_toVizLayer.csv');
 SELECT download.insert_viz_csv();
