99f99baf5ad17cf9c9a5a4243df342e4a417be31
0e1 <0e1@riseup.net>
2023-03-29 17:03:05 -0300

    Simplifica e move pro fim load/ini

--- src/download-step2-ini.sql
+++
@@ -1,7 +0,0 @@
--- dl.digital-guard
-SELECT download.load_dldg_csv('/var/gits/_dg/preserv/data/redirs/fromDL_toFileServer.csv');
-SELECT download.insert_dldg_csv();
-
--- Data VisualiZation
-SELECT download.load_viz_csv('/var/gits/_dg/preserv/data/redirs/fromCutLayer_toVizLayer.csv');
-SELECT download.insert_viz_csv();
--- src/optim-step2-ini.sql
+++ src/optim-step2-ini.sql
@@ -3,3 +3,10 @@ SELECT optim.insert_donor_pack(t) FROM unnest(ARRAY[     'BO','BR','CL','CO','EC
 SELECT optim.load_codec_type();
 SELECT optim.insert_codec_type();
 
+-- dl.digital-guard
+SELECT download.load_dldg_csv('/var/gits/_dg/preserv/data/redirs/fromDL_toFileServer.csv');
+SELECT download.insert_dldg_csv();
+
+-- Data VisualiZation
+SELECT download.load_viz_csv('/var/gits/_dg/preserv/data/redirs/fromCutLayer_toVizLayer.csv');
+SELECT download.insert_viz_csv();
