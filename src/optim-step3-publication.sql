a530a819d47737d3ffb5b47daf30a6d9fed32b81
0e1 <0e1@riseup.net>
2023-09-11 14:43:04 -0300

    mv step5 to step3.

--- src/optim-step3-publication.sql
+++ src/optim-step3-publication.sql
@@ -304,3 +304,100 @@ COMMENT ON FUNCTION optim.publicating_index_page
   IS 'Generate index in markdown file for preservDataViz pages.'
 ;
 -- SELECT optim.publicating_index_pagemd('/tmp/pg_io/index_teste2.md','/var/gits/_dg/preservDataViz/src/preservCutGeo/index_page_markdown.mustache');
+
+----------------------
+
+CREATE or replace VIEW optim.vw01generate_list AS
+SELECT scope_label, isolevel, pacotes || coalesce(jsonb_build_object('filtered_files',filtered_files),'{}'::jsonb) AS pacotes
+FROM
+(
+  SELECT pack_id, scope_label, isolevel, jsonb_build_object('legalName',legalName,'isunpublished',(CASE WHEN lower(MAX(regexp_replace(packtpl_info->>'uri_objtype', '[\s+]|\-', '', 'g'))) = 'email' THEN TRUE ELSE FALSE END),
+  'pack_number', MAX(pack_number), 'path_preserv_git', MAX(path_preserv_git), 'local_serial_formated', MAX(local_serial_formated), 'pacotes',jsonb_agg(pf2.*)) AS pacotes
+  FROM
+  (
+    SELECT pf.*
+    FROM optim.vw01full_packfilevers pf
+    ORDER BY scope_label, legalName,pack_id,pack_item
+  ) pf2
+  GROUP BY country_id, local_serial, scope_osm_id, scope_label, shortname, vat_id, legalName, wikidata_id, url, donor_info, kx_vat_id, isolevel, pack_id
+  ORDER BY scope_label, legalName
+) r
+LEFT JOIN optim.vw01filtered_files s
+ON r.pack_id = s.pack_id
+;
+
+
+CREATE or replace VIEW optim.vw02generate_list AS
+SELECT jsonb_build_object('paises',jsonb_agg(jsonb_build_object('scope_label', scope_label, 'iso1', iso1, 'iso3', iso3, 'jurisd', jurisd))) AS y
+FROM
+(
+    SELECT COALESCE(s.scope_label,r.scope_label) AS scope_label, iso3, iso1, jurisd
+    FROM
+    (
+      SELECT scope_label, jsonb_agg(iso3) AS iso3
+      FROM
+      (
+        SELECT split_part(scope_label,'-',1)  AS scope_label, jsonb_build_object('jurisd',scope_label,'doadores',jsonb_agg(pacotes)) AS iso3
+        FROM optim.vw01generate_list
+        WHERE scope_label LIKE '%-%-%'
+        GROUP BY scope_label
+        ORDER BY scope_label
+      ) m
+      GROUP BY scope_label
+    ) s
+    FULL OUTER JOIN
+    (
+      SELECT scope_label, jsonb_agg(pacotes) AS iso1
+      FROM optim.vw01generate_list
+      WHERE isolevel = 1
+      GROUP BY scope_label
+    ) r
+    ON s.scope_label = r.scope_label,
+    LATERAL
+    (
+      SELECT jsonb_agg(h.*)->0 AS jurisd
+      FROM optim.jurisdiction h
+      WHERE  abbrev = COALESCE(s.scope_label,r.scope_label) AND isolevel=1
+    ) v
+) t
+;
+
+CREATE or replace VIEW optim.vw03generate_list_hash AS
+SELECT jsonb_build_object('pacotes',jsonb_agg(r.*)) AS y
+FROM
+(
+  SELECT legalName, scope_label, hashedfname, hashedfname_7, pack_number, local_serial_formated, path_preserv_git, info
+  FROM optim.vw01full_packfilevers pf
+  ORDER BY hashedfname
+) r
+;
+
+CREATE or replace FUNCTION optim.generate_list(
+	p_fileref text,
+	p_template text DEFAULT '/var/gits/_dg/preserv/src/list_jurisd.mustache'
+) RETURNS text  AS $f$
+    SELECT volat_file_write(p_fileref, jsonb_mustache_render(pg_read_file(p_template), y)) AS output_write
+    FROM optim.vw02generate_list
+    ;
+$f$ language SQL VOLATILE;
+COMMENT ON FUNCTION optim.generate_list
+  IS 'Generate list page.'
+;
+-- SELECT optim.generate_list('/tmp/pg_io/list_jurisd_with_filtered_markdown.txt',    '/var/gits/_dg/preserv/src/list_jurisd.mustache');
+-- SELECT optim.generate_list('/tmp/pg_io/list_jurisd_without_filtered_markdown.txt', '/var/gits/_dg/preserv/src/list_jurisd_without_filtered.mustache');
+-- SELECT optim.generate_list('/tmp/pg_io/list_jurisd_with_filtered_mediawiki.txt',   '/var/gits/_dg/preserv/src/list_jurisd_mediawiki.mustache');
+-- SELECT optim.generate_list('/tmp/pg_io/list_jurisd_without_filtered_mediawiki.txt','/var/gits/_dg/preserv/src/list_jurisd_without_filtered_mediawiki.mustache');
+
+CREATE or replace FUNCTION optim.generate_list_hash(
+	p_fileref text,
+	p_template text DEFAULT '/var/gits/_dg/preserv/src/list_hash.mustache'
+) RETURNS text  AS $f$
+    SELECT volat_file_write(p_fileref, jsonb_mustache_render(pg_read_file(p_template), y)) AS output_write
+    FROM optim.vw03generate_list_hash
+    ;
+$f$ language SQL VOLATILE;
+COMMENT ON FUNCTION optim.generate_list_hash
+  IS 'Generate list page.'
+;
+-- SELECT optim.generate_list_hash('/tmp/pg_io/list_hash_markdown.txt');
+-- SELECT optim.generate_list_hash('/tmp/pg_io/list_hash_mediawiki.txt','/var/gits/_dg/preserv/src/list_hash_wiki.mustache');
